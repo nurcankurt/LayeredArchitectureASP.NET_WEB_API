fc421ffd1d75bec2b1d8195197e8b6fce84061ef
Nurcan Kurt <nkurt430@gmail.com>
2023-07-11 01:01:05 +0300

    Create ApiTest class

--- MyProject.Test/ApiTest.cs
+++ MyProject.Test/ApiTest.cs
@@ -1,11 +1,46 @@
+using Moq;
+using MyProject.BusinessLogicLayer;
+using MyProject.DataAccessLayer.Models;
+
 namespace MyProject.Test
 {
-    public class UnitTest1
+    public class ApiTest
     {
         [Fact]
-        public void Test1()
+        public async Task MapGet_ReturnsPersonList()
         {
+            //Arrange
+            var mockPersonService = new Mock<IPersonService>();
+            mockPersonService.Setup(service => service.GetAllPeople()).Returns(GetTestPersonList());
+   
 
+            // Act 
+           
+            //Assert
+
+        }
+
+        private List<Person> GetTestPersonList() {
+            var persons = new List<Person>
+            {
+            
+                    new Person
+                    {
+                        Id = 1,
+                        FirstName = "Nurcan",
+                        LastName = "Kurt",
+                        Email = "nkurt430"
+                    },
+                    new Person
+                    {
+                        Id = 2,
+                        FirstName = "Test",
+                        LastName = "Test",
+                        Email = "Test"
+                    }
+               
+            };
+            return persons;
         }
     }
 }
